d36d92f0b9f929c768127b0ce56f2428fd47afce
Amala Devi <amala.devi@bankbazaar.com>
2019-05-31 10:53:14 +0530

    Added checkbox control

--- scripts/setupTests.js
+++ scripts/setupTests.js
@@ -1,6 +1,6 @@
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import {isEqual} from "WCHelpers/Utils";
+import {isEqual} from "../src/helpers/Utils.js";
 
 configure({ adapter: new Adapter() });
 
---
+++ src/controls/Checkbox.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default class Checkbox extends React.PureComponent {
+    static propTypes = {
+        onChange: PropTypes.func,
+        checked: PropTypes.bool,
+        name: PropTypes.string,
+        value: PropTypes.any.isRequired,
+    };
+
+    render() {
+        return (
+            <span className="checkboxBtnStyle">
+                <input type="checkbox" value={this.props.value}
+                    name={this.props.name} 
+                    onChange={this.onChangeHandler} 
+                    checked={this.props.checked}
+                    ref={(i) => this.instance = i}/>
+                <span className="firstIcon"></span>
+                <span className="secondIcon">
+                    <span className="secondIconStem"></span>
+                    <span className="secondIconKick"></span>
+                </span>
+            </span>
+        );
+    }
+
+    onChangeHandler = (e) => {
+        if (this.props.onChange) {
+            this.props.onChange(e, {checked: this.instance.checked, value: this.props.value});
+        }
+    }
+}
